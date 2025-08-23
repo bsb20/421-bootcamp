ae295032187840cdb5443de662935e267a2b6e97
Zhongrui Chen <jcpwfloi@gmail.com>
2025-08-23 13:40:25 -0400

    bootcamp1

--- src/bootcamp1.cpp
+++ src/bootcamp1.cpp
@@ -2,14 +2,11 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <sstream>
 
 int main() {
-    std::string sentence = "the quick brown fox jumps over the lazy dog the quick fox";
-    std::stringstream ss(sentence);
-    std::string word;
     std::vector<std::string> words_vector;
-    while (ss >> word) {
+    std::string word;
+    while (std::cin >> word) {
         words_vector.push_back(word);
     }
 
